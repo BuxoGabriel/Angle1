2cea63ac9dec59ef9f439c5edaa7e3252723fa4d
Gabriel <gabrielbuxo1@gmail.com>
2022-06-08 03:28:56 -0400

    Created a barebones website using Angular framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticatorComponent } from './authenticator/authenticator.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticatorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/authenticator/authenticator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authenticator',
+  templateUrl: './authenticator.component.html',
+  styleUrls: ['./authenticator.component.sass']
+})
+export class AuthenticatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
